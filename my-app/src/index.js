237223c2229156ce750dfbf3c372455f95c4bb46
isaackhlam <97761927+isaackhlam@users.noreply.github.com>
2022-06-05 02:54:27 +0800

    Build Input Area

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,29 +3,87 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class CommentInput extends Component{
-    render() {
-        return (
-            <div>CommentInput</div>
-        )
-    }
+	constructor() {
+		super();
+		this.state = {
+			username: "",
+			content: "",
+		};
+	}
+
+	handleUsernameChange(event) {
+		this.setState({
+			username: event.target.value
+		})
+	}
+
+	handleContentChange(event) {
+		this.setState({
+			content: event.target.value
+		})
+	}
+
+	handleSubmit(event) {
+		if (this.props.onSubmit) {
+			const {username, content} = this.state;
+			this.props.onSubmit({username, content});
+		}
+		this.setState({content:""});
+	}
+
+  render() {
+    return (
+      <div className='comment-input'>
+				<div className='comment-field'>
+					<span className='comment-field-name'>User Name:</span>
+					<div className='comment-field-input'>
+						<input 
+							value={this.state.username}
+							onChange={this.handleUsernameChange.bind(this)}
+						/>
+					</div>
+				</div>
+				<div className='comment-field'>
+					<span className='comment-field-name'>Comment:</span>
+					<div className='comment-field-input'>
+						<textarea 
+							value={this.state.content}
+							onChange={this.handleContentChange.bind(this)}
+						/>
+					</div>
+				</div>
+				<div className='comment-field-button'>
+					<button
+						onClick={this.handleSubmit.bind(this)}>
+						Submit
+					</button>
+				</div>
+      </div>
+    )
+  }
 }
 
 class CommentList extends Component {
-    render() {
-        return (
-            <div>CommentList</div>
-        )
-    }
+  render() {
+    return (
+      <div>CommentList</div>
+    )
+  }
 }
 class CommentApp extends Component {
-    render() {
-        return (
-            <div className = "wrapper">
-                <CommentInput />
-                <CommentList />
-            </div>
-        )
-    }
+handleSubmitComment (comment) {
+	console.log(comment)
+}
+
+  render() {
+    return (
+      <div className = "wrapper">
+        <CommentInput 
+					onSubmit={this.handleSubmitComment.bind(this)} />
+        <CommentList />
+      </div>
+    )
+  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
